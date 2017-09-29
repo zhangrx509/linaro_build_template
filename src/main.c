a3bc1ae1688514b2db214caf58f8db495607f298
renxuan.zhang <renxuan.zhang@nio.com>
2017-09-29 11:00:06 +0800

    Add a stub main.c

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ * Copyright (c) 2017-2017 NIO Autonomous Driving
+ * All rights reserved.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+    while (1)
+    {
+    }
+    return 0;
+}
